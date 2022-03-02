201443d40dc75ec1b9986ce0cfc2a4d8b3cb04d1
Rishi Sharma <rishisharma@Rishis-MacBook-Air.local>
2022-03-02 18:56:10 +0530

    added meta

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,9 @@ import Head from "next/head";
 const Header = ({ query, data }) => {
   return (
     <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="google-site-verification" content={process.env.GOOGLE} />
       <title>
         Acetor
         {query === ""
@@ -22,7 +25,74 @@ const Header = ({ query, data }) => {
             : `Get Magnet links for ${query}`
         }
       />
-      {/* <meta name="keywords" content={ d data.map((d) => d.name)} /> */}
+
+      <meta
+        name="description"
+        content={
+          query === ""
+            ? "Get Magnet/Torrent links for everything !!!"
+            : Object.keys(data).length === 15
+            ? data.name
+            : data.map((m) => m.name)
+        }
+      />
+
+      <meta
+        name="keywords"
+        content={
+          Object.keys(data).length === 15 ? data.name : data.map((m) => m.name)
+        }
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://acetor.live" />
+      <meta
+        property="og:title"
+        content={
+          query === ""
+            ? "Acetor"
+            : "Acetor-" + query.charAt(0).toUpperCase() + query.slice(1)
+        }
+      />
+      <meta
+        property="og:description"
+        content={
+          query === ""
+            ? "Get Magnet/Torrent links for everything !!!"
+            : Object.keys(data).length === 15
+            ? data.descr
+            : data.map((m) => m.descr)
+        }
+      />
+      <meta
+        property="og:image"
+        content="https://github.com/Rishi-Sharma2002/TorrentWebsite/blob/master/src/AceTor.png?raw=true"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://acetor.live" />
+      <meta
+        property="twitter:title"
+        content={
+          query === ""
+            ? "Acetor"
+            : "Acetor-" + query.charAt(0).toUpperCase() + query.slice(1)
+        }
+      />
+      <meta
+        property="twitter:description"
+        content={
+          query === ""
+            ? "Get Magnet/Torrent links for everything !!!"
+            : Object.keys(data).length === 15
+            ? data.descr
+            : data.map((m) => m.descr)
+        }
+      />
+      <meta
+        property="twitter:image"
+        content="https://github.com/Rishi-Sharma2002/TorrentWebsite/blob/master/src/AceTor.png?raw=true"
+      />
     </Head>
   );
 };
