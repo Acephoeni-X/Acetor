6885aa0868a7852c9702541c72a1988b11c0ad4a
Rishi Sharma <sharma.rishix02@gmail.com>
2023-05-07 20:35:19 +0530

    search functionality env added

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -10,7 +10,7 @@ export default Query;
 export async function getServerSideProps(context) {
   const { query } = context.query;
   let data = await (
-    await fetch(`${process.env.NEXT_PUBLIC_SEARCH}${query}`)
+    await fetch(`${process.env.NEXT_PUBLIC_SEARCH}${query} `)
   ).json();
   return {
     props: {
