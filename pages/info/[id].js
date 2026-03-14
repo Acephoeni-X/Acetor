45909095895fa151aff350f39636abfc529ee803
Acephoeni-X <sharma.rishix02@gmail.com>
2026-03-14 16:58:05 +0530

    added caching as we are getting rate limited

--- pages/info/[id].js
+++ pages/info/[id].js
@@ -2,6 +2,10 @@ import React from "react";
 import Imdb from "../../components/Imdb";
 import Info from "../../components/Info";
 import print_magnet from "../../services/magnet";
+import NodeCache from "node-cache";
+
+
+const cache = new NodeCache({ stdTTL: 600 }); // 10 min
 
 const Id = ({ data }) => {
   let magnet = print_magnet(data.info_hash, "Acetor-" + data.name);
@@ -19,8 +23,17 @@ const Id = ({ data }) => {
 export default Id;
 
 export async function getServerSideProps(context) {
+  console.log('Inside this')
   const { id } = context.query;
-  let data = await (await fetch(`${process.env.NEXT_PUBLIC_INFO}${id}`)).json();
+  const cached = cache.get(id);
+  let data;
+  if (cached) {
+    data = cached;
+  }else{
+    data = await (await fetch(`${process.env.NEXT_PUBLIC_INFO}${id}`)).json()
+    cache.set(id, data);
+  }
+  console.log('data', data)
   if (data.imdb) {
     let imdbData = await (await fetch(process.env.OMDB_API + data.imdb)).json();
     Object.assign(data, imdbData);
