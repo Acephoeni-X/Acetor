d997e0cfb7d058876927cd092cc608b3705b7ba5
Acephoeni-X <sharma.rishix02@gmail.com>
2026-04-10 09:19:35 +0530

    updated next config for build issues

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["github.com", "ipfs.infura.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
   },
 };
 
