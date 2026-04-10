e045675f81c4629b5e7ed0f1730cbee59245856f
Acephoeni-X <sharma.rishix02@gmail.com>
2026-04-10 08:47:18 +0530

    made it client side

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -1,16 +1,12 @@
-import React from "react";
+import { useRouter } from "next/router";
 import SearchBody from "../../components/SearchBody";
 
-const Query = ({ query }) => {
+const Query = () => {
+  const router = useRouter();
+  const queryParam = router.query.query;
+  const query = Array.isArray(queryParam) ? queryParam[0] : queryParam || "";
+
   return <SearchBody query={query} />;
 };
 
 export default Query;
-
-export async function getServerSideProps({ params }) {
-  return {
-    props: {
-      query: params?.query ?? "",
-    },
-  };
-}
